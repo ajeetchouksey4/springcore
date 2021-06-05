de69e17b0bd020847f1352fbce5d1c11643d869c
ajeetchouksey4 <ajeetchouksey4@gmail.com>
2021-06-05 20:10:24 +0530

    Interface Based Annotation SPring bean LifeCycle

---
+++ springcore/src/main/java/com/springcore/App.java
@@ -0,0 +1,21 @@
+package com.springcore;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/app-context.xml");
+
+		Employee emp = context.getBean(Employee.class);
+
+		System.out.println(emp);
+
+		System.out.println("+++++++++++++++++++++++++++++++");
+
+		System.out.println(emp);
+
+		context.registerShutdownHook();
+
+	}
+}
